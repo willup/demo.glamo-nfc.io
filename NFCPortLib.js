1a6446dcdc60e8f2e1e115c880548c31301dcf13
nhan.duong <nhan.duong@sotatek.com>
2022-11-30 13:42:09 +0700

    add comment

--- NFCPortLib.js
+++ NFCPortLib.js
@@ -923,7 +923,7 @@ class ReaderWriterBase {
       null != a &&
       a.mifareAuthentication
     ) {
-      if (null == a.key || 6 != a.key.length)
+      if (null == a.key || 16 != a.key.length)
       {
         console.log("nhan 8======")
         return (
