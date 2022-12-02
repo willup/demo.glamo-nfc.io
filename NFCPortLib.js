f12750454baf46b95a6550051614fce36537d393
nhan.duong <nhan.duong@sotatek.com>
2022-12-02 09:11:53 +0700

    add comment

--- NFCPortLib.js
+++ NFCPortLib.js
@@ -886,11 +886,6 @@ class ReaderWriterBase {
   async communicateThru(t, i, a) {
     console.log("nhan 1===== communicateThru input : ", t, i, a)
     console.log("nhan 4==== parameter: ", this.protocol, a, t)
-    if(i==100){
-      console.log("Nhan======= Read Binary")
-    }
-    // console.log("nhan 5===== checking parameter: ", this.protocol != ReaderWriterBase.PROTOCOL_ISO14443_3A, null == a, !a.mifareAuthentication,
-    // null == t,  )
     let o, s;
     if ((e("communicateThru begin"), this.bAbnormalState))
       return (
